7533953bc4ff5ac8aeb9fa36ee936f7c574194bc
Romain Beaumont <romain.rom1@gmail.com>
2023-06-25 23:37:18 +0200

    Add simple local spark example.

---
+++ examples/run_on_spark_local.py
@@ -0,0 +1,13 @@
+from cc2dataset import cc2dataset
+import os
+
+if __name__ == "__main__":
+    cc2dataset(
+        "/media/hd/tmp_output_cc2dataset",
+        wat_index_count=None,
+        wat_count=100,
+        master="local",
+        num_cores=16,
+        mem_gb=16,
+        source_cc_protocol="http"
+    )
