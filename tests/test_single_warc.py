d35fd42715a6fa245f00bcdb95b10a377aa2358e
Marianna <mariana13019940@gmail.com>
2023-09-26 13:16:25 +0200

    add WARC iframe test

--- tests/test_single_warc.py
+++ tests/test_single_warc.py
@@ -3,6 +3,7 @@ from cc2dataset import process_wat
 import pandas as pd
 
 test_url = "https://data.commoncrawl.org/crawl-data/CC-MAIN-2022-40/segments/1664030331677.90/wat/CC-MAIN-20220924151538-20220924181538-00000.warc.wat.gz"
+test_warc_url = "https://data.commoncrawl.org/crawl-data/CC-MAIN-2022-40/segments/1664030331677.90/warc/CC-MAIN-20220924151538-20220924181538-00000.warc.gz"
 
 
 def retry(f, n=3):
@@ -16,29 +17,45 @@ def retry(f, n=3):
             return retry(f, n - 1)
 
 
+cols = ["uid", "url", "alt", "cc_filename", "page_url"]
+
+
 def test_single_warc_image():
-    results = retry(lambda: process_wat(test_url, "image"))
-    df = pd.DataFrame(results, columns=["uid", "url", "alt"])
-    assert len(df) == 240033
-    assert df["uid"][3] == "ee8ab8628552d88a099129cf1a452745"
+    results = retry(lambda: process_wat(test_url, document_type="image"))
+    df = pd.DataFrame(results, columns=cols)
+    assert len(df) == 405232
+    assert df["uid"][3] == "2a64f921d7ced2fed91e82eeb56338cd"
+
+
+def test_single_warc_image_only():
+    results = retry(lambda: process_wat(test_url, "image_only"))
+    df = pd.DataFrame(results, columns=cols)
+    assert len(df) == 733994
+    assert df["uid"][3] == "331e6480d20ea2f51a5557ca7e20909a"
 
 
 def test_single_warc_audio():
     results = retry(lambda: process_wat(test_url, "audio"))
-    df = pd.DataFrame(results, columns=["uid", "url", "alt"])
-    assert len(df) == 721
-    assert df["uid"][3] == "fd38d5c43140dfda889566eddd8755c0"
+    df = pd.DataFrame(results, columns=cols)
+    assert len(df) == 927
+    assert df["uid"][3] == "5c835ccd44d718e0a95d74b4a2902dfe"
 
 
 def test_single_warc_text():
     results = retry(lambda: process_wat(test_url, "text"))
-    df = pd.DataFrame(results, columns=["uid", "url", "alt"])
-    assert len(df) == 6375
+    df = pd.DataFrame(results, columns=cols)
+    assert len(df) == 10552
     assert df["uid"][3] == "b485d42a0fad04a4e7e2fdb114e341c8"
 
 
 def test_single_warc_video():
     results = retry(lambda: process_wat(test_url, "video"))
-    df = pd.DataFrame(results, columns=["uid", "url", "alt"])
-    assert len(df) == 508
+    df = pd.DataFrame(results, columns=cols)
+    assert len(df) == 676
     assert df["uid"][3] == "a8f5837e354808f319d2a4899089090c"
+
+
+def test_single_warc_iframe():
+    results = retry(lambda: process_wat(test_warc_url, "iframe"))
+    df = pd.DataFrame(results, columns=cols)
+    assert len(df) == 16407
